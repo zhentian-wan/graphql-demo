209d6fc009fae73a0f637cce2d4a9471e8b14a42
Zhentian Wan <answer881215@gmail.com>
2016-12-28 09:21:04 +0800

    single query

---
+++ main.js
@@ -0,0 +1,42 @@
+const { graphql, buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+    type Video {
+        id: ID,
+        title: String,
+        duration: Int,
+        watched: Boolean
+    }
+    
+    type Query {
+        video: Video
+    }
+    
+    type Schema{
+        query: Query
+    }
+`);
+
+const resolvers = {
+    video : () => ({
+        id       : '1',
+        title    : 'bar',
+        duration : 180,
+        watched  : true
+    })
+};
+
+const query = `
+    query myFirstQuery {
+        video {
+            id,
+            title,
+            duration,
+            watched
+        }
+    }
+`;
+
+graphql(schema, query, resolvers)
+.then((result) => console.log(result))
+.catch(console.error)
